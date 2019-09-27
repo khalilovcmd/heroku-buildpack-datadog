73c7d1ee9a8c0c03d12780226ea3a68accb3944a
Ara Pulido <ara.pulido@datadoghq.com>
2019-09-27 11:01:44 +0200

    Delete integrations not used in Heroku as part of the compile process

--- bin/common.sh
+++ bin/common.sh
@@ -16,3 +16,11 @@ function indent() {
     *)      sed -u "$c";;
   esac
 }
+
+DELETE_INTEGRATIONS=(btrfs cassandra_nodetool ceph cisco_aci cockroachdb \
+consul coredns crio ecs_fargate envoy go-metro harbor hdfs_datanode hdfs_namenode \
+hive ibm_db2 ibm_mq ibm_was istio kube_apiserver_metrics kube_controller_manager \
+kube_dns kube_metrics_server kube_proxy kube_scheduler kubelet kubernetes_state \
+kyototycoon linkerd mapreduce marathon mesos_master mesos_slave nagios nfsstat \
+nginx_ingress_controller openldap openstack openstack_controller postfix \
+powerdns_recursor presto riakcs snmp sqlserver squid statsd tokumx vsphere yarn)
