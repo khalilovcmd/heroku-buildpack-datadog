ab49c9081e6f114407345b8b7c4b32d04afc7e48
Tarek Khalil <tarek.saber.se@gmail.com>
2019-10-11 11:50:42 +0100

    Update datadog.sh

--- extra/datadog.sh
+++ extra/datadog.sh
@@ -88,7 +88,7 @@ if [ -z "$DD_HOSTNAME" ]; then
     fi
 
     D="$(echo "$DYNO" | sed -e 's/[^a-zA-Z0-9.-]/-/g' -e 's/^-//g')"
-    export DD_HOSTNAME="$HAN"
+    export DD_HOSTNAME="$HAN.$D"
   else
     # Set the hostname to the dyno host
     DD_HOSTNAME="$(echo "$DYNOHOST" | sed -e 's/[^a-zA-Z0-9-]/-/g' -e 's/^-//g')"
